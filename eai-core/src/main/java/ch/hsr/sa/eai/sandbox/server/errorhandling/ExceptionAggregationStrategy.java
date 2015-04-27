0bd0aa3ddf4aa8d355e5685413628fb8aa3b166c
koelbener <lukas@koelbeners.ch>
2015-04-27 16:46:38 +0200

    add support for email notification on exception and record failures

---
+++ eai-core/src/main/java/ch/hsr/sa/eai/sandbox/server/errorhandling/ExceptionAggregationStrategy.java
@@ -0,0 +1,17 @@
+package ch.hsr.sa.eai.sandbox.server.errorhandling;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+
+public class ExceptionAggregationStrategy implements AggregationStrategy {
+
+	@Override
+	public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+		Object executionId = newExchange.getIn().getHeader("executionId");
+		newExchange.getIn().setBody(
+				"Processing failure in integration job " + executionId
+						+ ", please check the failure file failedRecords/" + executionId);
+		return newExchange; // no real aggregation needed
+	}
+
+}
