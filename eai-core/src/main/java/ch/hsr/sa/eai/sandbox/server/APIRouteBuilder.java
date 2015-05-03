f18c2056db6e839544932c3bb9f8b1caadffd46d
Michi Gysel <mgysel@hsr.ch>
2015-05-03 22:01:42 +0200

    first draft of eai batch processing

--- eai-core/src/main/java/ch/hsr/sa/eai/sandbox/server/APIRouteBuilder.java
+++ eai-core/src/main/java/ch/hsr/sa/eai/sandbox/server/APIRouteBuilder.java
@@ -1,6 +1,10 @@
 package ch.hsr.sa.eai.sandbox.server;
 
+import java.util.Properties;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import ch.hsr.sa.eai.sandbox.server.rest.api.JobResult;
@@ -9,17 +13,22 @@ import ch.hsr.sa.eai.sandbox.server.rest.api.Jobs;
 
 @Component("apiRouteBuilder")
 public class APIRouteBuilder extends RouteBuilder {
+	
+	@Autowired
+	private Properties properties;
 
 	@Override
 	public void configure() throws Exception {
-		rest("/jobs").id("restRouteOverview").get().outType(Jobs.class).to("restRouteManagement");
-		rest("/jobs/{jobName}").id("restRouteJobInfo").get().outType(JobStatus.class).to("restRouteStatus");
-		rest("/jobs/{name}").id("restRouteStartJob").post().outType(JobResult.class).to("restRouteStarter");
-		// the parameter name is different intentionally. camel does not allow
-		// two rest routes with the exact same uri.
-		from("jms:JobTrigger?exchangePattern=InOut").id("jmsRouteStarter").beanRef("jmsRouteStarter");
-		from("quartz2://resetMetricsTimer?cron={{metrics.reset.cron}}").id("resetMetrics").to(
-				"bean:metricHelper?method=resetMetrics");
+		if("true".equals(properties.get("rest.api.enabled"))) {
+			rest("/jobs").id("restRouteOverview").get().outType(Jobs.class).to("restRouteManagement");
+			rest("/jobs/{jobName}").id("restRouteJobInfo").get().outType(JobStatus.class).to("restRouteStatus");
+			rest("/jobs/{name}").id("restRouteStartJob").post().outType(JobResult.class).to("restRouteStarter");
+			// the parameter name is different intentionally. camel does not allow
+			// two rest routes with the exact same uri.
+			from("jms:JobTrigger?exchangePattern=InOut").id("jmsRouteStarter").beanRef("jmsRouteStarter");
+			from("quartz2://resetMetricsTimer?cron={{metrics.reset.cron}}").id("resetMetrics").to(
+					"bean:metricHelper?method=resetMetrics");
+		}
 
 	}
 
